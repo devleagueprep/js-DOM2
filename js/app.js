dad44b7b55351ceae112759e36a34d1aba263b88
acdoyle630@gmail.com <acdoyle630@gmail.com>
2017-01-23 18:58:19 -1000

    cleaned #8

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ Rating: 5 Stars*/
 
 var rate = "Rating: 5 Stars";
 var newRate = document.getElementById ('rating1');
-newRate.id = 'five';
+newRate.id = 'rate';
 newRate.innerHTML = rate;
 
 //2. Sunrise Restaurant
@@ -18,13 +18,13 @@ newRate.innerHTML = rate;
 Price: $$$
 Adress: 525 Kapahulu Ave, Honolulu, HI 96815*/
 var newPrice = "Price: $$$";
-var newAdd = 'Address: 525 Kapahulu Ave, Honolulu HI 96815';
+var newAddy = 'Address: 525 Kapahulu Ave, Honolulu HI 96815';
 
 var addprice = document.getElementById ('price2');
-addprice.innerHTML = newPrice;
+var addAddy = document.getElementById ('addy2');
 
-var addy = document.getElementById ('addy2');
-addy.innerHTML = newAdd;
+addprice.innerHTML = newPrice;
+addAddy.innerHTML = newAddy;
 //3. Ramen Nakamura
 /*Literally, where did the time go? Ramen Nakamura is missing the hours section! This mistake is timeless.
 
@@ -90,7 +90,7 @@ I'd leave a blank space for this oxtail soup!
 
 Append this element to the div id of "comment4"*/
 
-var comment = "I'd leave a blank space for this mutha fuckin soup";
+var comment = "I'd leave a blank space for this oxtail soup";
 var createComment = document.createElement ('p');
 createComment.id = 'update4';
 createComment.innerHTML = comment;
@@ -98,11 +98,6 @@ comment4.appendChild (createComment);
 
 //5. The Death Star Cafe
 /*The empire strikes back with a new and improved oxtail soup offering. Help the dark side win back customers by executing the following commands:*/
-var deathName = "The Death Star Cafe";
-var deathRating = "Rating: 2 Stars";
-var deathPrice = "Price: $$";
-var deathAddy = "Address: A glaxy far, far away.";
-var deatHours = "Hours: We never close.";
 
 
 /*Create the div elements with the following id's:
@@ -123,28 +118,35 @@ hours5: Hours: We never close.
 
 Append the div elements to the div id "text5"
 */
+var deathName = "The Death Star Cafe";
+var deathRating = "Rating: 2 Stars";
+var deathPrice = "Price: $$";
+var deathAddy = "Address: A glaxy far, far away.";
+var deatHours = "Hours: We never close.";
 
 var addDeathName = document.createElement ('div');
-addDeathName.id = 'name5';
-addDeathName.innerHTML = deathName;
-text5.appendChild (addDeathName);
-
 var addDeathRating = document.createElement ('div');
-addDeathRating.id = 'rating5';
-addDeathRating.innerHTML = deathRating;
-text5.appendChild (addDeathRating);
-
 var addDeathPrice = document.createElement('div');
-addDeathPrice.id = 'price5';
-addDeathPrice.innerHTML = deathPrice;
-text5.appendChild (addDeathPrice);
-
 var addDeathHours = document.createElement ('div');
-addDeathHours.id = 'hours5';
-addDeathHours.innerHTML = deatHours;
-text5.appendChild (addDeathHours);
-
 var addDeathAddy = document.createElement ('div');
+
+addDeathName.id = 'name5';
+addDeathRating.id = 'rating5';
+addDeathPrice.id = 'price5';
+addDeathHours.id = 'hours5';
 addDeathAddy.id = 'addy5';
+
+addDeathName.innerHTML = deathName;
+addDeathRating.innerHTML = deathRating;
+addDeathPrice.innerHTML = deathPrice;
+addDeathHours.innerHTML = deatHours;
 addDeathAddy.innerHTML = deathAddy;
+
+text5.appendChild (addDeathName);
+text5.appendChild (addDeathRating);
+text5.appendChild (addDeathPrice);
+text5.appendChild (addDeathHours);
 text5.appendChild (addDeathAddy);
+
+
+
